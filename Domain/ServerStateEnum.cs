111fc4f68ae74c87709c7dfdf4999866061af389
ChengWei <chengwei.tw.1994@gmail.com>
2023-04-23 01:46:30 +0800

    修改server程式

---
+++ Domain/ServerStateEnum.cs
@@ -0,0 +1,33 @@
+﻿namespace MySocketServer.Domain
+{
+    /// <summary>
+    /// Server 狀態
+    /// </summary>
+    public enum ServerStateEnum
+    {
+        /// <summary>
+        /// 正在啟動
+        /// </summary>
+        OnStarting,
+
+        /// <summary>
+        /// 服務中
+        /// </summary>
+        OnService,
+
+        /// <summary>
+        /// 正在關閉
+        /// </summary>
+        OnClosing,
+
+        /// <summary>
+        /// 已停止服務
+        /// </summary>
+        OutOfService,
+
+        /// <summary>
+        /// 發生未預期的錯誤
+        /// </summary>
+        Error
+    }
+}
--- Domain/SocketSvr.cs
+++ Domain/SocketSvr.cs
@@ -6,10 +6,11 @@ namespace MySocketServer.Domain
 {
     public class SocketSvr : IDisposable
     {
-        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-        private TcpListener? _tcpListener = null;
-        private ConcurrentBag<Task> _tasks = new ConcurrentBag<Task>();
-        private IPAddress _ipAddress;
+        private ServerStateEnum _state = ServerStateEnum.OutOfService;
+        private readonly IPAddress _ipAddress;
+        private CancellationTokenSource? _cancellationTokenSource;
+        private ConcurrentBag<Task>? _tasks;
+        private TcpListener? _tcpListener;
         public string IP { get; private set; }
         public int Port { get; private set; }
 
@@ -20,17 +21,112 @@ namespace MySocketServer.Domain
             _ipAddress = IPAddress.Parse(IP);
         }
 
+        private bool _canStart => _state == ServerStateEnum.OutOfService;
+
+        /// <summary>
+        /// 啟動
+        /// </summary>
         public void Start()
         {
-            // TODO: if tcpListener is open then close it.
-            _tcpListener = new TcpListener(_ipAddress, Port);
-            _tcpListener.Start();
+            if (!_canStart) return;
+
+            try
+            {
+                _state = ServerStateEnum.OnStarting;
+
+                _cancellationTokenSource = new CancellationTokenSource();
+                _tasks = new ConcurrentBag<Task>();
+
+                // TODO: if tcpListener is open then close it.
+                _tcpListener = new TcpListener(_ipAddress, Port);
+                _tcpListener.Start();
+
+                StartedSuccessfully();
+            }
+            catch (Exception ex)
+            {
+                StartFailed();
+            }
         }
 
+        /// <summary>
+        /// 啟動失敗
+        /// </summary>
+        private void StartFailed()
+        {
+            _state = ServerStateEnum.OutOfService;
+        }
+
+        /// <summary>
+        /// 服務已成功啟動
+        /// </summary>
+        private void StartedSuccessfully()
+        {
+            _state = ServerStateEnum.OnService;
+        }
+
+        private bool _canShutdown => _state == ServerStateEnum.OnService;
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        /// <returns></returns>
         public async Task Shutdown()
         {
-            _cancellationTokenSource.Cancel();
-            await Task.WhenAll(_tasks.ToArray());
+            if (!_canShutdown) return;
+
+            try
+            {
+                _state = ServerStateEnum.OnClosing;
+
+                _tcpListener?.Stop();
+
+                _cancellationTokenSource?.Cancel();
+
+                if (_tasks is not null)
+                    await Task.WhenAll(_tasks.ToArray());
+
+                ShutdownSuccessfully();
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine($"\n{nameof(OperationCanceledException)} thrown\n");
+
+                // TODO:
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"\n{nameof(Exception)} thrown {ex}\n");
+
+                ShutdownError();
+            }
+            finally
+            {
+                _cancellationTokenSource?.Dispose();
+            }
+
+            // Display status of all tasks.
+            if (_tasks is not null)
+            {
+                foreach (var task in _tasks)
+                    Console.WriteLine("Task {0} status is now {1}", task.Id, task.Status);
+            }
+        }
+
+        /// <summary>
+        /// 停止時發生錯誤
+        /// </summary>
+        private void ShutdownError()
+        {
+            _state = ServerStateEnum.Error;
+        }
+
+        /// <summary>
+        /// 服務已成功停止
+        /// </summary>
+        private void ShutdownSuccessfully()
+        {
+            _state = ServerStateEnum.OutOfService;
         }
 
         #region Implement IDisposable
